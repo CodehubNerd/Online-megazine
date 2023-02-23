15482594d454b9d8762ffcf039fc8c622d79b0e1
CodehubNerd <111436414+CodehubNerd@users.noreply.github.com>
2023-02-23 20:25:24 +0200

    pushing

--- js/app.js
+++ js/app.js
@@ -1,54 +1,100 @@
 
-var url = window.location.href;
+const storyCathupDivs = document.querySelectorAll(".story-cathup");
+
+storyCathupDivs.forEach((div) => {
+  const shareIcon = div.querySelector(".single-icon");
 
 
-document.getElementById("share-btn").addEventListener("click", function () {
-    console.log("clicked")
-  var platform = prompt("Enter the name of the platform you want to share on (Facebook, Twitter, LinkedIn)");
+  shareIcon.addEventListener("click", () => {
+    const url = window.location.href;
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?url=${url}`;
+    window.open(facebookUrl, "_blank");
+    window.open(twitterUrl, "_blank");
+  });
+});
 
-  if (platform === null || platform === "") {
-    return;
-  }
 
-  if (platform.toLowerCase() === "facebook") {
-    window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url));
-  } else if (platform.toLowerCase() === "twitter") {
-    window.open("https://twitter.com/share?url=" + encodeURIComponent(url));
-  } else if (platform.toLowerCase() === "linkedin") {
-    window.open("https://www.linkedin.com/shareArticle?mini=true&url=" + encodeURIComponent(url));
-  } else {
-    alert("Invalid platform name");
+
+const articlessDiv = document.getElementById('articless');
+
+
+articlessDiv.addEventListener('click', () => {
+ 
+  const storyCathupDivs = document.querySelectorAll('.story-cathup');
+
+  for (let i = 0; i < storyCathupDivs.length; i++) {
+    const storyCathupDiv = storyCathupDivs[i];
+    if (storyCathupDiv.id === 'articles') {
+      storyCathupDiv.style.display = 'block';
+    } else {
+      storyCathupDiv.style.display = 'none';
+    }
   }
 });
 
 
 
+// get the essays div element
+const essaysDiv = document.getElementById('essays');
 
-document.getElementById("articless").addEventListener("click", function () {
-    document.getElementById("articles").style.display = "block";
-    document.getElementById("short-stories").style.display = "none";
-    document.getElementById("essay").style.display = "none";
-    document.getElementById("Poems").style.display = "none";
-  });
-  
-  document.getElementById("storiess").addEventListener("click", function() {
-    document.getElementById("articles").style.display = "none";
-    document.getElementById("short-stories").style.display = "block";
-    document.getElementById("essay").style.display = "none";
-    document.getElementById("Poems").style.display = "none";
-  });
-  
-  document.getElementById("essays").addEventListener("click", function() {
-    document.getElementById("articles").style.display = "none";
-    document.getElementById("short-stories").style.display = "none";
-    document.getElementById("essay").style.display = "block";
-    document.getElementById("Poems").style.display = "none";
-  });
-  
-  document.getElementById("poemss").addEventListener("click", function() {
-    document.getElementById("articles").style.display = "none";
-    document.getElementById("short-stories").style.display = "none";
-    document.getElementById("essay").style.display = "none";
-    document.getElementById("Poems").style.display = "block";
-  });
-  
+// add a click event listener to the essays div
+essaysDiv.addEventListener('click', () => {
+  // get all the story-cathup div elements
+  const storyCathupDivs = document.querySelectorAll('.story-cathup');
+
+  // loop over the story-cathup divs and check if they have the ID 'essay'
+  for (let i = 0; i < storyCathupDivs.length; i++) {
+    const storyCathupDiv = storyCathupDivs[i];
+    if (storyCathupDiv.id === 'essay') {
+      // display the story-cathup div with the ID 'essay'
+      storyCathupDiv.style.display = 'block';
+    } else {
+      // hide the other story-cathup divs
+      storyCathupDiv.style.display = 'none';
+    }
+  }
+});
+
+
+const poemssDiv = document.getElementById('poemss');
+
+// add a click event listener to the essays div
+poemssDiv.addEventListener('click', () => {
+  // get all the story-cathup div elements
+  const storyCathupDivs = document.querySelectorAll('.story-cathup');
+
+  // loop over the story-cathup divs and check if they have the ID 'essay'
+  for (let i = 0; i < storyCathupDivs.length; i++) {
+    const storyCathupDiv = storyCathupDivs[i];
+    if (storyCathupDiv.id === 'Poems') {
+      // display the story-cathup div with the ID 'essay'
+      storyCathupDiv.style.display = 'block';
+    } else {
+      // hide the other story-cathup divs
+      storyCathupDiv.style.display = 'none';
+    }
+  }
+});
+
+
+
+const storiessDiv = document.getElementById('storiess');
+
+// add a click event listener to the essays div
+storiessDiv.addEventListener('click', () => {
+  // get all the story-cathup div elements
+  const storyCathupDivs = document.querySelectorAll('.story-cathup');
+
+  // loop over the story-cathup divs and check if they have the ID 'essay'
+  for (let i = 0; i < storyCathupDivs.length; i++) {
+    const storyCathupDiv = storyCathupDivs[i];
+    if (storyCathupDiv.id === 'storie') {
+      // display the story-cathup div with the ID 'essay'
+      storyCathupDiv.style.display = 'block';
+    } else {
+      // hide the other story-cathup divs
+      storyCathupDiv.style.display = 'none';
+    }
+  }
+});
